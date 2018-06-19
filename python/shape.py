555bf7382e6abd4e738f8d5f5105dcd5bd256e55
Eric Penrod <emp923@gmail.com>
2018-06-19 08:58:32 -0400

    messing with inheritance

---
+++ python/shape.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# messing around with classes
+
+# Base class
+class Shape(object):
+    def setWidth(self, w):
+        self.width = w
+    def setHeight(self, h):
+        self.height = h
+
+# Derived class
+class Rectangle(Shape):
+    def getArea(self):
+        return self.width * self.height
+
+# Another derived class
+class Triangle(Shape):
+    def getArea(self):
+        return self.width * self.height / 2
+
+Rect = Rectangle
+Tri = Triangle
+
+Rect.setWidth(4)
+Rect.setHeight(8)
+Tri.setWidth(4)
+Tri.setHeight(8)
+
+# Print the area of the object
+print "Total area of rectangle: " + Rect.getArea()
+print "Total area of triangle: " + Tri.getArea()
+
+# END
