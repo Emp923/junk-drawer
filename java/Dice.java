a1bf107e439722d6b1f810c0d8da584805e047ed
Eric Penrod <emp923@gmail.com>
2018-06-30 21:36:51 -0400

    java dice game

--- java/Dice.java
+++ java/Dice.java
@@ -12,9 +12,29 @@ public class Dice {
         System.out.println("---------\n| *   * |\n|   *   |\n| *   * |\n---------");
     }
 
+    public static void four() {
+        System.out.println("---------\n| *   * |\n|       |\n| *   * |\n---------");
+    }
+
+    public static void three() {
+        System.out.println("---------\n|     * |\n|   *   |\n| *     |\n---------");
+    }
+
+    public static void two() {
+        System.out.println("---------\n|     * |\n|       |\n| *     |\n---------");
+    }
+
+    public static void one() {
+        System.out.println("---------\n|       |\n|   *   |\n|       |\n---------");
+    }
+
     public static void main(String[] args) {
 
         six();
         five();
+        four();
+        three();
+        two();
+        one();
     }
 }
