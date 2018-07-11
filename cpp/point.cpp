8df9e5032431dc4905b305779e14995f1193eb07
Eric Penrod <emp923@gmail.com>
2018-07-11 08:36:22 -0400

    book and pointer

---
+++ cpp/point.cpp
@@ -0,0 +1,19 @@
+// playing with pointers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var1;
+    char var2[10];
+
+    cout << "Address of var1 variable: ";
+    cout << &var1 << endl;
+
+    cout << "Address of var2 variable: ";
+    cout << &var2 << endl;
+
+    return 0;
+}
+
+// END
