a854159a51fb006ce320275c2f3b2a9fb7a1b88d
Eric Penrod <emp923@gmail.com>
2018-08-01 10:01:57 -0400

    javascript from lecture

--- js/test.js
+++ js/test.js
@@ -5,8 +5,12 @@ var person = {
     lastName: "Penrod",
     fullName: function() {
         return this.firstName + " " + this.lastName;
+    },
+    hello: function() {
+        return "Hello World!";
     }
 }
 
 console.log("This is a test.");
 console.log(person.fullName());
+console.log(person.hello());
